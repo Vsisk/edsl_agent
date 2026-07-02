6c62ae33e986dae8aef7d43232a7ab7a08b094eb
Vsisk <wzyvsss@gmail.com>
2026-07-02 21:07:44 +0800

    refactor: remove legacy namingsql ranking pipeline

--- agent/naming_sql_selector/__init__.py
+++ agent/naming_sql_selector/__init__.py
@@ -1,8 +1,6 @@
-from .models import (NamingSqlParamProfile, NamingSqlProfile, NamingSqlSelectRequest,
-    NamingSqlSelectResponse)
+from .models import NamingSqlSelectRequest, NamingSqlSelectResponse
 from .selector import NamingSqlSelector
 from .plan_validator import validate_naming_sql_plan
-from .profile_builder import NamingSqlProfileBuilder
 
-__all__ = ["NamingSqlParamProfile", "NamingSqlProfile", "NamingSqlProfileBuilder",
-    "NamingSqlSelectRequest", "NamingSqlSelectResponse", "NamingSqlSelector", "validate_naming_sql_plan"]
+__all__ = ["NamingSqlSelectRequest", "NamingSqlSelectResponse", "NamingSqlSelector",
+    "validate_naming_sql_plan"]
